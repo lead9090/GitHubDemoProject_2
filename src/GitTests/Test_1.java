cc5566cec1c592ee0b25e80978b1537893d13781
sebas <sebas@192.168.0.102>
2022-09-02 19:31:09 +0100

    This my first commit

---
+++ src/GitTests/Test_1.java
@@ -0,0 +1,20 @@
+package GitTests;
+
+public class Test_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =10,b=10;
+		if(a==b) {
+		System.out.println("both are same");
+		}
+		else{
+		System.out.println("both are not same");
+		}
+				
+	}
+
+
+	}
+
+
