f269c995b15fa336f1cf3c5f3d4c1f398548f928
sebas <sebas@192.168.0.102>
2022-09-02 22:38:24 +0100

    committing test 2 branchX

---
+++ src/GitTests/Test_2.java
@@ -0,0 +1,22 @@
+package GitTests;
+
+public class Test_2 {
+
+	public static void main(String[] args) {
+		byte A=10,b=20;
+		 int c=A+b;
+				
+		System.out.println(c);	
+				
+		
+		
+				
+		System.out.println("welcome");
+		System.out.println("jesus my lord");
+		System.out.println("binu");
+		
+		
+	}
+
+}
+
